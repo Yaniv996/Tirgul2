057fc3c439dfec6e34bcc7ec118a287028b60c37
Yaniv Shatil <yanivst76@gmail.com>
2024-08-03 18:23:13 +0300

    Update ThreadCheckArray.java

--- ThreadCheckArray/src/ThreadCheckArray.java
+++ ThreadCheckArray/src/ThreadCheckArray.java
@@ -103,4 +103,6 @@ public class ThreadCheckArray implements Runnable
 	public long getRuntime() {
         return endTime - startTime;
 	}
+	
+	//////////////////////CHANGEEEEEEEEEEEEE////////////////////////////
 }
